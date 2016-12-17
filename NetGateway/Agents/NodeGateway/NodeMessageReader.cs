00bed3c89bc358feb5b3bb5f957595a59aceeabc
Valery De Smedt <valery@indri.be>
2016-12-17 19:29:24 +0100

    New async NodeMessageReader

---
+++ NetGateway/Agents/NodeGateway/NodeMessageReader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Sql;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HelloHome.NetGateway.Agents.NodeGateway.Domain;
+using HelloHome.NetGateway.Agents.NodeGateway.Encoders;
+using HelloHome.NetGateway.Agents.NodeGateway.Parsers;
+using HelloHome.NetGateway.Configuration;
+using NLog;
+
+namespace HelloHome.NetGateway.Agents.NodeGateway
+{
+    public class NodeMessageReader
+    {
+        static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        readonly SerialPort _serial;
+        readonly List<IMessageParser> _parsers;
+        readonly List<IMessageEncoder> _encoders;
+
+        public NodeMessageReader(ISerialConfigurationProvider serialConfig, IEnumerable<IMessageEncoder> encoders)
+        {
+            _encoders = encoders.ToList();
+            _parsers = new List<IMessageParser>
+            {
+                new CommentParser(),
+                new NodeStartedParser(),
+                new PulseReportParser(),
+                new EnvironmentReportParser(),
+                new NodeInfoReportParser(),
+                new ParseAllParser(),
+            };
+            _serial = new SerialPort(serialConfig.Port, 115200, Parity.None, 8, StopBits.One);
+        }
+
+        public async Task SendAsync(OutgoingMessage message, CancellationToken cancellationToken)
+        {
+            var encoder = _encoders.FirstOrDefault(_ => _.CanEncode(message));
+            if (encoder == null)
+                throw new ApplicationException($"No encoder found for {message.GetType().Name}");
+            var bytes = encoder.Encode(message);
+            await _serial.BaseStream.WriteAsync(bytes, 0, bytes.Length, cancellationToken);
+            cancellationToken.ThrowIfCancellationRequested();
+            await _serial.BaseStream.WriteAsync(new byte[] {0x0D, 0x0A}, 0, 2, cancellationToken);
+        }
+
+        private const int BufferSize = 100;
+        readonly byte[] _buffer = new byte[BufferSize];
+        int _currentBufferIndex = 0;
+
+        /// <summary>
+        /// Return a message or null if timout passes without a complete message ending with Eof can be found
+        /// </summary>
+        /// <param name="cancelationToken"></param>
+        /// <returns></returns>
+        public async Task<IncomingMessage> ReadAsync(CancellationToken cancelationToken)
+        {
+            // at start, some left overs might still be at the begining of the buffer,
+            // currentIndex can therefore be greater than 0
+            byte[] eof = {0x0D, 0x0A};
+
+            var eofMatchCharCount = 0;
+            while (eofMatchCharCount < eof.Length)
+            {
+                var byteCount = await ReadAsync(_buffer, _currentBufferIndex, BufferSize - _currentBufferIndex, cancelationToken);
+                if (byteCount <= 0)
+                    return null;
+
+                _currentBufferIndex += byteCount;
+                while (eofMatchCharCount < eof.Length && byteCount > 0)
+                {
+                    if (_buffer[_currentBufferIndex - byteCount] == eof[eofMatchCharCount])
+                        eofMatchCharCount++;
+                    else
+                        eofMatchCharCount = 0;
+                    byteCount--;
+                }
+
+                if (eofMatchCharCount != eof.Length)
+                    continue;
+
+                var msgBytes = new byte[_currentBufferIndex - byteCount];
+                for (var i = 0; i < _currentBufferIndex - byteCount; i++)
+                    msgBytes[i] = _buffer[i];
+
+                var parser = _parsers.FirstOrDefault(_ => _.CanParse(msgBytes));
+                var msg = parser.Parse(msgBytes);
+                for (var i = 0; i < byteCount; i++)
+                    _buffer[i] = _buffer[_currentBufferIndex - byteCount + i];
+                _currentBufferIndex = byteCount;
+                return msg;
+            }
+            return null;
+        }
+
+        protected virtual async Task<int> ReadAsync(byte[] buffer, int offset, int count,
+            CancellationToken cancellationToken)
+        {
+            if (!_serial.IsOpen)
+            {
+                _serial.Open();
+                _serial.BaseStream.ReadTimeout = 10;
+            }
+            return await _serial.BaseStream.ReadAsync(buffer, offset, count, cancellationToken);
+        }
+    }
+}
---
+++ UnitTests/Agents/NodeGateway/NodeMessageReaderTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using HelloHome.NetGateway.Agents.NodeGateway;
+using HelloHome.NetGateway.Agents.NodeGateway.Domain;
+using HelloHome.NetGateway.Agents.NodeGateway.Encoders;
+using HelloHome.NetGateway.Configuration;
+using HelloHome.NetGateway.Configuration.AppSettings;
+using Moq;
+using Xunit;
+
+namespace UnitTests.Agents.NodeGateway
+{
+    public class TestableNodeMessageReader : NodeMessageReader
+    {
+        private readonly Queue<byte[]> _byteSeries = new Queue<byte[]>();
+        private static readonly Mock<ISerialConfigurationProvider> ConfigMock = new Mock<ISerialConfigurationProvider>();
+
+        public TestableNodeMessageReader(IEnumerable<IMessageEncoder> encoders)
+            : base(ConfigMock.Object, encoders)
+        {
+        }
+
+        protected override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            if(_byteSeries.Count == 0)
+                return await Task.FromResult(0);
+            var bytes = _byteSeries.Dequeue();
+            for (var i = 0; i < Math.Min(bytes.Length, count); i++)
+                buffer[offset + i] = bytes[i];
+            return await Task.FromResult(bytes.Length);
+        }
+
+        public void EnqueueBytes(byte[] bytes)
+        {
+            _byteSeries.Enqueue(bytes);
+        }
+    }
+
+    public class NodeMessageReaderTests
+    {
+        private readonly TestableNodeMessageReader _sut;
+
+        public NodeMessageReaderTests()
+        {
+            _sut = new TestableNodeMessageReader(new List<IMessageEncoder>());
+
+        }
+
+        [Fact]
+        public async void no_eof_returns_null()
+        {
+            _sut.EnqueueBytes(new byte[] { 0x00, 0x10, 0x20 });
+            var msg = await _sut.ReadAsync(new CancellationToken());
+            Assert.Null(msg);
+        }
+
+        [Fact]
+        public async void returns_message_when_eof_in_first_baytearray()
+        {
+            _sut.EnqueueBytes(new byte[] { 0x12, 65, 66, 67, 0x0D, 0x0A });
+            var msg = await _sut.ReadAsync(new CancellationToken());
+            Assert.NotNull(msg);
+        }
+
+        [Fact]
+        public async void returns_message_when_eof_next_baytearrays()
+        {
+            _sut.EnqueueBytes(new byte[] { 0x12, 65, 66, 67});
+            _sut.EnqueueBytes(new byte[] { 0x0D, 0x0A });
+            var msg = await _sut.ReadAsync(new CancellationToken());
+            Assert.NotNull(msg);
+        }
+
+        [Fact]
+        public async void can_read_node_started_twice()
+        {
+            _sut.EnqueueBytes(new byte[] { 0x12, 0X00, 0x0A, 0 + 1 << 2, 0x02, 0x01 });
+            _sut.EnqueueBytes(new byte[] { 0x0D, 0x0A, 0x12, 0X00 });
+            _sut.EnqueueBytes(new byte[] { 0x0A, 0 + 1 << 2, 0x03, 0x01, 0x0D, 0x0A });
+            var msg1 = await _sut.ReadAsync(new CancellationToken());
+            Assert.True(msg1 is PulseReport);
+            Assert.Equal(2, ((PulseReport)msg1).SubNode);
+            var msg2 = await _sut.ReadAsync(new CancellationToken());
+            Assert.True(msg2 is PulseReport);
+            Assert.Equal(3, ((PulseReport)msg2).SubNode);
+        }
+        [Fact]
+        public async void can_read_node_started()
+        {
+            _sut.EnqueueBytes(new byte[] { 0x12, 0X00, 0x0A, 0 + 1 << 2, 0x02, 0x01 });
+            _sut.EnqueueBytes(new byte[] { 0x0D, 0x0A, 40, 41 });
+            var msg = await _sut.ReadAsync(new CancellationToken());
+            Assert.True(msg is PulseReport);
+        }
+    }
+}
