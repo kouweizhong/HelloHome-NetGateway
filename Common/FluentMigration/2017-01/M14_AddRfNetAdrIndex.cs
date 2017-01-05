86afd6d7b6895325d56ead14b3ea8c180111c36d
Valery De Smedt <valery@indri.be>
2017-01-05 22:00:11 +0100

    Add rfAddress/rfNetwork unique index

---
+++ Common/FluentMigration/2017-01/M14_AddRfNetAdrIndex.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using FluentMigrator.Expressions;
+
+namespace HelloHome.Common.FluentMigration
+{
+    [HhMigration(2017,1,5, 21, 57)]
+    public class M14_AddRfNetAdrIndex : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.UniqueConstraint("UC_Node_RfNetworkAddress")
+                .OnTable("Node")
+                .Columns("rfAddress", "rfNetwork");
+        }
+    }
+}
