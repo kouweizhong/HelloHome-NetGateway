58ece4c839b9f4862052edc99ea0a930a0859db1
Valery De Smedt <valery@indri.be>
2017-04-14 22:00:14 +0200

    Adding NodeCOnfig

---
+++ Common/FluentMigration/2017-02/M19_AddConfigForEnvNode.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace HelloHome.Common.FluentMigration
+{
+    [HhMigration(2017,2,5,21,20)]
+    public class M19_AddConfigForEnvNode : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("NodeConfig")
+                .AddColumn("Sleeps").AsBoolean().NotNullable().WithDefaultValue(false)
+                .AddColumn("NodeInfoFreq").AsInt16().NotNullable().WithDefaultValue(60*60)
+                .AddColumn("EnvironmentFreq").AsInt16().Nullable()
+                .AddColumn("RestartCheckFreq").AsInt16().NotNullable().WithDefaultValue(2*60*60)
+                .AddColumn("Hal1Pin").AsInt16().Nullable()
+                .AddColumn("vInTriggerPin").AsInt16().Nullable()
+                .AddColumn("vInMeasurePin").AsInt16().Nullable()
+                .AddColumn("Hal2Pin").AsInt16().Nullable()
+                .AddColumn("Dry1Pin").AsInt16().Nullable()
+                .AddColumn("Dry2Pin").AsInt16().Nullable()
+                .AddColumn("SIEnabled").AsBoolean().NotNullable().WithDefaultValue(false)
+                .AddColumn("BMPEnabled").AsBoolean().NotNullable().WithDefaultValue(false);
+        }
+    }
+}
--- NetGateway/MessageChannel/Domain/Commands/NodeConfigCommand.cs
+++ NetGateway/MessageChannel/Domain/Commands/NodeConfigCommand.cs
@@ -3,35 +3,20 @@ using HelloHome.NetGateway.MessageChannel.Domain.Base;
 
 namespace HelloHome.NetGateway.MessageChannel.Domain.Commands
 {
-	[DataContract]
     public class NodeConfigCommand : OutgoingMessage
 	{
-		[DataMember]
 	    public long Signature { get; set; }
-
-	    [DataMember]
 	    public byte NewRfAddress { get; set; }
-
-	    [DataMember]
 	    public byte Hal1Pin { get; set; }
-
-	    [DataMember]
 	    public byte Hal2Pin { get; set; }
-
-	    [DataMember]
 	    public byte DryPin { get; set; }
-
-	    [DataMember]
 	    public byte VInTriggerPin { get; set; }
-
-	    [DataMember]
 	    public byte VInMeasurePin { get; set; }
-
-	    [DataMember]
 	    public bool SiEnable { get; set; }
-
-	    [DataMember]
 	    public bool BmpEnable { get; set; }
+	    public byte RestartCheckFreq { get; set; }
+	    public byte NodeInfoFreq { get; set; }
+	    public byte EnvironmentFreq { get; set; }
 	}
 
 
